329ac70590be2d9aee3c3ca9aef06814e65d492c
Matt <brimstone@the.narro.ws>
2014-10-27 19:14:38 -0400

    Adding main from example.

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+import (
+"fmt"
+"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+fmt.Fprintf(w, "<team></team>")
+}
+
+func main() {
+http.HandleFunc("/", handler)
+http.ListenAndServe(":80", nil)
+}
