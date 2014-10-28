a65c2705450f4e9b38441258a487bbec8f4bb61f
Matt <brimstone@the.narro.ws>
2014-10-28 12:06:20 -0400

    Adding flags and error output from last night.

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
 )
 
+var teamName = flag.String("team", "brimstone", "Team name")
+var listen = flag.String("listen", ":80", "Address and port for httpd")
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<team></team>")
+	fmt.Fprintf(w, "<team>%s</team>", *teamName)
 }
 
 func main() {
+	flag.Parse()
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":80", nil)
+	log.Printf("Preparing to listening on %s for team %s", *listen, *teamName)
+	err := http.ListenAndServe(*listen, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
